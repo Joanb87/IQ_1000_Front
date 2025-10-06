90aabbc043e508ce9b620992fe27eb85a76ea8e0
Joanb87 <joan.benavides@iq-online.com>
2025-10-06 08:47:29 -0500

    prueba despliegue 2

--- IQ_MIL/src/config/api.ts
+++ IQ_MIL/src/config/api.ts
@@ -1,3 +1,3 @@
-// URL base del backend para centralizar su uso en el frontend
+// URL base del backend 
 export const API_URL = "https://api-case-flow-73469724535.us-central1.run.app"
 // "http://localhost:8080"
